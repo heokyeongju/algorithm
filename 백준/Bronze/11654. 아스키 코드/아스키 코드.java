ac2066f2ef07ee9a5715348ec7a09884d61f09b2
Kyeong Ju <beewt@naver.com>
2022-12-04 23:47:52 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void solution() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char c = br.readLine().charAt(0);
+        System.out.println((int)c);
+    }
+
+    public static void main(String[] args) throws Exception{
+        solution();
+    }
+}
