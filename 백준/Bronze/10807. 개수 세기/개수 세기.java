485d63f025ca4de82e32533115a036f60ae025ad
Kyeong Ju <beewt@naver.com>
2022-12-29 14:36:16 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void solution() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int result = 0;
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int v = Integer.parseInt(br.readLine());
+        
+
+        for(int i=0; i<n; i++){
+            if(arr[i] == v){
+                result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) throws Exception{
+        solution();
+    }
+}
