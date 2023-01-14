e6bee39f12b266e4df2816ad1a8a44a8b9e4c28e
Kyeong Ju <beewt@naver.com>
2023-01-14 16:23:59 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.18 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        for(int i = 0; i < t.length()-p.length()+1; i++){
+            String sub = t.substring(i, i+p.length());
+            if(Long.parseLong(sub) - Long.parseLong(p) <= 0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
